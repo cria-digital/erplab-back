330a66ea0e3c127d6b19794ce588be4efd5c17aa
Diego Soek <diegosoek@gmail.com>
2025-09-06 18:59:59 -0300

    Ajuste para gerar o build

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,13 +4,13 @@ import { config } from 'dotenv';
 // Carrega as variáveis de ambiente
 config();
 
-export const AppDataSource = new DataSource({
+const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'nestuser',
   password: process.env.DB_PASSWORD || 'nestpass',
-  database: process.env.DB_DATABASE || 'erplab',
+  database: process.env.DB_DATABASE || 'erplab_db',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
   synchronize: false,
